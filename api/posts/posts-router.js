3425e5f4a099da0e18559f4094279a5e413ce42d
luisbryandr <luisbdominguezr@gmail.com>
2022-01-11 16:16:09 -0500

    building out server and routing imports

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
 // implement your posts router here
+
+const express = require('express');
+const Posts = require('./posts-model');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('GET request')
+})
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postRouter = require('../api/posts/posts-router')
+
+const server = express();
+
+server.use(express.json());
+
+server.use('api/posts', postRouter);
+
+server.use('*', (req, res) => {
+    res.send(`
+    <h1>Posts DataBase API</h1>
+    <p>Welcome to the default page of API </p>`)
+})
+
+module.exports = server;
